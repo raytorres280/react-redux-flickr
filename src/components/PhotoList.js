8020a8c3d70b78544dcdf9c7116e16e28d6c237e
raytorres280 <raytorres280@gmail.com>
2017-09-02 20:18:41 -0400

    app done, added cardrds and responsiveness

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -12,13 +12,48 @@ class PhotoList extends React.Component {
 		};
 	}
 
+	componentWillReceiveProps(newProps) {
+		this.setState({photos: newProps.photos});
+	}
+
+	photoGrid() {
+		const photos = this.state.photos.slice();
+		const gridItems = photos.map((photo) => (<div className="col-lg-2 col-md-4 col-sm-12">
+								<div className="card"><img className="card-photo" src={photo.media.m} /></div>
+							</div>));
+
+		let grid = <div></div>;
+		let gridRows = [];
+		while(gridItems.length > 0) {
+			let gridRow = <div className="row"></div>;
+				gridRow = React.cloneElement(
+					gridRow,
+					null,
+					[gridItems.pop(), gridItems.pop(), gridItems.pop(), gridItems.pop(), gridItems.pop(), gridItems.pop()]
+				);
+				gridRows.push(gridRow);
+				console.log(gridRow);
+		}
+		grid = React.cloneElement(
+			grid,
+			null,
+			gridRows
+		);
+
+		return grid;
+	}
 	render() {
 		return(
 				<div>
 					<h1>Photos list....</h1>
 					<SearchBar />
 					<Link to="profile" className="btn btn-primary btn-lg">profile</Link>
-
+					{/*{this.state.photos.map((photo) => (*/}
+						{/*<div className="card">*/}
+							{/*<img src={photo.media.m} />*/}
+						{/*</div>*/}
+					{/*))}*/}
+					{this.photoGrid()}
 				</div>
 		);
 	}
--- src/reducers/photoReducer.js
+++ src/reducers/photoReducer.js
@@ -2,7 +2,8 @@ export default function photoReducer(state = [], action) {
 	switch(action.type) {
 		case 'GET_PHOTOS_SUCCESS':
 			console.log('action found in reducer');
-			return Object.assign({}, state, action.photos);
+			let newState = Object.assign([], state, action.photos);
+			return newState;
 		default:
 			return state;
 	}
